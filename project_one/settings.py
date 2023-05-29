38a5b008cdd47961cf1bfa2cff54a72f1d87a20a
younes bettahar <younesdz03@gmail.com>
2023-05-29 11:52:06 +0200

    commit config setting

--- project_one/settings.py
+++ project_one/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)f)5u=2#3$^vvidt$5z*!v1tl15r2_zde$vs32e0z!fcy94*-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost' , 'younesdzdev.pythonanywhere.com']
 
 
 # Application definition
@@ -125,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR ,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2
